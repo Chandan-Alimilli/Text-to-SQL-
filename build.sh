11a5caae8c5ca0eab8dc061d4621cba3db8ee324
Chandan-Alimilli <chandanalimilli248@gmail.com>
2025-06-24 17:52:55 +0530

    build

--- build.sh
+++ build.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+# Update and install system-level build tools required by blis and spacy
+apt-get update && apt-get install -y build-essential gcc python3-dev
+
+# Upgrade pip
+pip install --upgrade pip
+
 # Install Python dependencies
 pip install -r backend/requirements.txt
 
 # Download the spaCy model
 python -m spacy download en_core_web_sm
+
+
+uvicorn main:app --host=0.0.0.0 --port=10000
