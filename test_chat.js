5601fd8b54c9eddfc14fd7ca210ba920832a1ee0
Chandan-Alimilli <chandanalimilli248@gmail.com>
2025-08-10 22:51:56 +0530

    pip

--- test_chat.js
+++ test_chat.js
@@ -1,4 +1,1 @@
-pip install en_core_web_sm-3.7.1-py3-none-any.whl
-
-
-pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1-py3-none-any.whl
+pip install "snowflake-snowpark-python[pandas]"
