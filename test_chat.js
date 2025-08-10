92f7ce46452ea9cfa2e64bc7d123bbeed5ce15d8
Chandan-Alimilli <chandanalimilli248@gmail.com>
2025-08-10 22:34:37 +0530

    ,,

--- test_chat.js
+++ test_chat.js
@@ -1,1 +1,4 @@
+pip install en_core_web_sm-3.7.1-py3-none-any.whl
+
+
 pip install https://github.com/explosion/spacy-models/releases/download/en_core_web_sm-3.7.1/en_core_web_sm-3.7.1-py3-none-any.whl
